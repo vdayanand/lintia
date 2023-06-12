d557ab574cfde0e1142210d40ea58ceb53b8f460
vdayanand <venkatesh@juliacomputig.com>
2023-06-13 00:01:56 +0530

    new algorithm

--- build.rs
+++ build.rs
@@ -1,11 +1,14 @@
 use std::path::PathBuf;
 
 fn main() {
-    let dir: PathBuf = ["tree-sitter-julia", "src"].iter().collect();
 
-    cc::Build::new()
-        .include(&dir)
-        .file(dir.join("parser.c"))
-        .file(dir.join("scanner.c"))
-        .compile("tree-sitter-julia");
+    let dir: PathBuf = ["tree-sitter-julia", "src"].iter().collect();
+    let mut b = cc::Build::new();
+    b.warnings(false);
+    b.include(&dir)
+    .file(dir.join("parser.c"))
+    .file(dir.join("scanner.c"))
+    .compile("tree-sitter-julia");
+
+    println!("cargo:rerun-if-changed=build.rs");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,82 @@
-use tree_sitter::{Language, Parser};
+use tree_sitter::{Language, Parser, Node};
 
 extern "C" {
     fn tree_sitter_julia() -> Language;
 }
 
-fn print_node(node: &tree_sitter::Node, source_code: &str, indent: usize) {
-    let separator = "  ".repeat(indent);
+fn print_node(node: &tree_sitter::Node) {
+    println!("Kind: {}", node.kind());
+    println!("has error: {}", node.has_error());
+    println!("Child count: {}", node.child_count());
+}
 
-    println!("{}- node: {}", separator, node.to_sexp());
-    println!("{}  kind: {}", separator, node.kind());
-    println!(
-        "{}  start: {}, {}",
-        separator,
-        node.start_position().row,
-        node.start_position().column
-    );
-    println!(
-        "{}  end: {}, {}",
-        separator,
-        node.end_position().row,
-        node.end_position().column
-    );
-    let chunk: String = source_code
-        .chars()
-        .skip(node.start_byte())
-        .take(node.end_byte() - node.start_byte())
-        .collect();
-    println!("{}  content: {}", separator, chunk);
+fn row(node: &Node, src: &String) -> usize{
+    let p =  node.start_position();
+    return p.row;
+}
+fn col(node: &Node, src: &String) -> usize{
+    let p =  node.start_position();
+    return p.column;
+}
+fn analyse(node: &Node, src: &String, env: &Vec<String>) {
+   if let Ok(val) = node.utf8_text(src.as_bytes()) {
+        let sym = val.to_string();
+        if !env.contains(&sym.to_string()) {
+            println!("Undefined symbol {} found at {}:{}, ", sym, row(&node, src), col(&node, src));
+        }
+    }
+}
+fn eval(node: &Node, src: &String, env: &Vec<String>){
+    match node.kind() {
+        "source_file" => {
+            let mut tc = node.walk();
+            for child in node.named_children(&mut tc) {
+                eval(&child, src, env);
+            }
+        },
+        "assignment_expression" => {
+            if let Some(rnode) = node.named_child(1) {
+                eval(&rnode, src, env)
+            }
+        },
+        "parenthesized_expression" => {
+            if let Some(rnode) = node.named_child(0) {
+                eval(&rnode, src, env)
+            }
+        },
+        "number" => (),
+        "binary_expression" => {
+            if let Some(firstnode) = node.named_child(0) {
+                if firstnode.kind() == "identifier" {
+                    analyse(&firstnode, src, env)
+                }
+                else {
+                    eval(&firstnode, src, env)
+                }
+            }
+            if let Some(secondnode) = node.named_child(1) {
+                if secondnode.kind() == "identifier" {
+                    analyse(&secondnode, src, env)
+                }
+                else {
+                    eval(&secondnode, src, env);
+                }
+            }
+        },
+        _ => {
+            println!("Unimplemented kind {}", node.kind());
+        }
+    };
 }
 
 fn main() {
     let mut parser = Parser::new();
     let language = unsafe { tree_sitter_julia() };
     parser.set_language(language).unwrap();
-    let source_code = "f(x)=1";
-    let mut indent = 0;
+    let source_code = "a = x + (y + 1)";
     let tree = parser.parse(source_code, None).unwrap();
-    let mut tree_cursor = tree.walk();
-    let mut level = 0;
-    let mut child_printed = false;
-
-    loop {
-        if !child_printed && tree_cursor.goto_first_child() {
-            indent += 1;
-            print_node(&tree_cursor.node(), &source_code, indent);
-        } else if tree_cursor.goto_next_sibling() {
-            child_printed = false;
-            print_node(&tree_cursor.node(), &source_code, indent);
-            if tree_cursor.goto_first_child() {
-                indent += 1;
-                level += 1;
-                print_node(&tree_cursor.node(), &source_code, indent);
-            }
-        } else {
-            if level > 0 {
-                tree_cursor.goto_parent();
-                child_printed = true;
-                level -= 1;
-                indent -= 1;
-            } else {
-                break;
-            }
-        }
-    }
+    let root_node = tree.root_node();
+    let env = Vec::<String>::new();
+    //let env = vec![String::from("x")];
+    eval(&root_node, &String::from(source_code), &env);
 }
